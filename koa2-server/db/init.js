b7b5db99eba154f8beeb20b0dde44b5f8666953c
YAYO严 <15720826383@163.com>
2020-04-24 16:35:26 +0800

    21.koa2通过mongoose连接mongodb数据库 使用promise提高连接的健壮性

--- koa2-server/db/init.js
+++ koa2-server/db/init.js
@@ -11,24 +11,43 @@ const dbUrl = "mongodb://localhost:27017/vue-koa-mall";
 exports.connect = () => {
     //连接数据库
     mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true});
+    //重新连接的次数
+    let maxConnectTimes = 0;
 
-    //增加数据库的监听事件
-    //当数据库断开的时候
-    mongoose.connection.on("disconnected", () => {
-        console.log("*************************数据库断开**************************");
-        mongoose.connect(dbUrl);
+    //返回一个promise，数据库正常连接的时候才会继续往下执行，如果不正常连接，是不会继续执行的
+    return new Promise((resolve, reject) => {
+        //增加数据库的监听事件
+        //当数据库断开的时候
+        mongoose.connection.on("disconnected", () => {
+            console.log("*************************数据库断开**************************");
+            if (maxConnectTimes <= 3) {
+                maxConnectTimes++;
+                mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true});
+            } else {
+                reject();
+                throw new Error("数据库出现异常，程序无法搞定，请人为修理...")
+            }
+
+        });
+
+        //当数据库发生错误的时候
+        mongoose.connection.on("error", (err) => {
+            console.log("*************************数据库错误**************************");
+            if (maxConnectTimes <= 3) {
+                maxConnectTimes++;
+                mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true});
+            } else {
+                reject(err);
+                throw new Error("数据库出现异常，程序无法搞定，请人为修理...")
+            }
+        });
+
+        //数据库打开连接的时候（数据库连接成功的时候）
+        mongoose.connection.on("open", () => {
+            console.log("*************************mongodb数据库连接成功**************************");
+            resolve();
+        })
     });
-
-    //当数据库发生错误的时候
-    mongoose.connection.on("error", () => {
-        console.log("*************************数据库错误**************************");
-        mongoose.connect(dbUrl);
-    })
-
-    //数据库打开连接的时候（数据库连接成功的时候）
-    mongoose.connection.on("open", () => {
-        console.log("*************************mongodb数据库连接成功**************************");
-    })
 
 
 };
